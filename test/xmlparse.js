06295bbbf720f9508d435e150d816062f7820bea
Joey.Ma <majunjiev@gmail.com>
2012-11-04 13:24:05 +0800

    1.implement a host.xml parser example

--- test/xmlparse.js
+++ test/xmlparse.js
@@ -1,6 +1,9 @@
 var xml = require('node-xml');
 
+var host = {};
+
 var parser = new xml.SaxParser(function (cb) {
+  var current;
   cb.onStartDocument(function () {
     console.log('start to parse xml document');
   });
@@ -8,8 +11,20 @@ var parser = new xml.SaxParser(function (cb) {
     console.log('finish the xml document parsing');
   });
   cb.onStartElementNS(function (elem, attrs, prefix, uri, namespaces) {
-    console.log('=> Started: ' + elem);
+    current = elem;
+  });
+  cb.onEndElementNS(function (elem, prefix, uri) {
+    
+  });
+  cb.onCharacters(function (chars) {
+    if (chars !== '') {
+      host[current] = chars;
+    }
   });
 });
 
 parser.parseFile('host.xml');
+
+setTimeout(function () {
+  console.log(host);
+}, 5000);
