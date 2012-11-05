bf33e215d80b867f133336b5b5ce98cdf29bf4b5
Joey.Ma <majunjiev@gmail.com>
2012-11-05 23:03:15 +0800

    modify the testcase for client class

--- test/client_test.js
+++ test/client_test.js
@@ -2,7 +2,7 @@ var client = require('../lib/client.js');
 
 var aClient = new client.Client('neoadmin:33333sdsf', 'http://10.1.81.235:2633/RPC2');
 
-aClient.call('neo.hostpool.info', null, function (oneResponse) {
+aClient.call('neo.host.info', [1], function (oneResponse) {
   console.log(oneResponse.success);
   console.log(oneResponse.msg);
 });
