e211901db05c80258e16c5852c31443cb940cdc0
Joey.Ma <majunjiev@gmail.com>
2012-11-07 16:21:02 +0800

    modify the client test and xmlrpc test file

--- test/client_test.js
+++ test/client_test.js
@@ -1,8 +1,8 @@
-var client = require('../lib/client.js');
+var client = require('../lib/client');
 
-var aClient = new client.Client('neoadmin:33333sdsf', 'http://10.1.81.235:2633/RPC2');
+var aClient = new client.Client('neoadmin:encyptedpasswd', 'http://10.1.81.235:2633/RPC2');
 
-aClient.call('neo.host.info', [1], function (oneResponse) {
+aClient.call('hostpool.info', [], function (oneResponse) {
   console.log(oneResponse.success);
   console.log(oneResponse.msg);
 });
--- test/xmlrpc_test.js
+++ test/xmlrpc_test.js
@@ -1,12 +1,20 @@
 var xmlrpc = require('xmlrpc');
+var select = require('xpath.js');
+var dom = require('xmldom');
 
 // Creates an XML-RPC client. Passes the host information on where to
 // make the XML-RPC calls.
-var client = xmlrpc.createClient({ host: '10.1.81.235', port: 2633, path: '/RPC2'});
+var client = xmlrpc.createClient('http://10.1.81.235:2633/RPC2');
+
 
 // Sends a method call to the XML-RPC server
 client.methodCall('neo.hostpool.info', ['neoadmin:33333sdsf'], function (error, value) {
   // Results of the method response
-  console.log('Method response for Action: ' + value[1]);
+  var doc = new dom.DOMParser().parseFromString(value[1]);
+  var nodes = select(doc, "HOST");
+  for (var i in nodes) {
+    console.log(nodes[i].localName + ": " + nodes[i].firstChild.data);
+    console.log("node: " + nodes[i].toString());  
+  }
 });
 
